dc80e522a0bde1c21bfc7ea175789c6b1f88d55c
Rommy156 <ROMILOLAADEPOJU@GMAIL.COM>
2026-02-27 12:16:17 -0500

    add object spawner and object placeholder

--- RacSim/Assets/Scripts/ItemSpawner.cs
+++ RacSim/Assets/Scripts/ItemSpawner.cs
@@ -1,43 +1,69 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class ItemSpawner : MonoBehaviour
 {
-    [Header("Spawn Settings")]
-    public GameObject itemPrefab;
-    public int maxItems = 10;
-    public Vector3 spawnAreaSize = new Vector3(20f, 1f, 20f);
-    private int currentItemCount = 0;
+    //an array to hold variable of all transform spawn points 
+    public Transform[] spawnPoints;
+    //reference to treasure prefab , which is a gameObject our player will be collecting
+    public GameObject treasurePrefab;
+    //reference to the UI text for coin coounting
+    public TextMeshProUGUI counterText;
+    // -1 means "does not exist" it is -1 because nothing has been spawned yet.
+    private int lastSpawnIndex = -1;
+    //variable to store how much coin has been collected
+    private int counter;
 
-    private void Start()
+    void Start()
     {
-        SpawnInitialItems();
+        //player has 0 coins at the start of the game
+        counter = 0;
+        //call updateCounter function
+        UpdateCounter();
+        //call SpawTreasure function
+        SpawnTreasure();
     }
 
-    void SpawnInitialItems()
+    //This function gets called on Start() to spawn the first item. It will also have to be called in OnTreasureCollected()
+    void SpawnTreasure()
     {
-        for (int i = 0; i < maxItems; i++)
+        //temp variable to store our new spawn point
+        int newSpawnIndex;
+        do
         {
-            SpawnItem();
+            //the range of where we want our next spawn points (min,max)
+            newSpawnIndex = Random.Range(0, spawnPoints.Length);
+        } while (newSpawnIndex == lastSpawnIndex);
+        lastSpawnIndex = newSpawnIndex;
+        //instantiate the treasurePrefab at the new spawn point's position and rotation
+        Instantiate(treasurePrefab, spawnPoints[newSpawnIndex].position, Quaternion.identity);
+    }
+
+    //A public function can accessed by other scripts. INTERESTING.
+    public void OnTreasureCollected()
+    {
+        //increment coin counter if treasureCollected and call UpdateCounter(), SpawnTreasure(), GameOver();
+        counter++;
+        UpdateCounter();
+        SpawnTreasure();
+        GameOver();
+
+    }
+
+    void UpdateCounter()
+    {
+        //update counterText UI and display text
+       // counterText.text = "Counter: " + counter.ToString();
+    }
+
+    void GameOver()
+    {
+        //if the counter reaches 10 load the gameOver scene
+        if (counter == 10)
+        {
+            SceneManager.LoadScene("GameOver");
         }
     }
-    void SpawnItem()
-    {
-        Vector3 randomPosition = new Vector3(
-        Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
-        0f,
-        Random.Range(-spawnAreaSize.z / 2, spawnAreaSize.z / 2)
-        );
-        Vector3 spawnPosition = transform.position + randomPosition;
-        Instantiate(itemPrefab, spawnPosition, Quaternion.identity);
-        currentItemCount++;
-    }
 
-    public void itemCollected()
-    {
-        currentItemCount--;
-        if (currentItemCount < maxItems)
-        {
-            SpawnItem();
-        }
-    }
-    
 }
